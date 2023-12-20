1bf9cfc4f2d9af7f828beddb3abc7319f7e8362e
Jared Elliott <bootcamp@jaredclt.com>
2023-12-20 12:36:01 -0500

    After class time for mini project.

---
+++ 05_28-Mini Project/assets/javascript/script.js
@@ -0,0 +1,45 @@
+let inputName = '';
+let inputType = '';
+let inputDue = '';
+
+console.log("Start of JS doc loaded");
+
+let displayDateTime = setInterval(function() {
+    var currDateTime = dayjs().format('MMM DD, YYYY') + " at " + dayjs().format('HH:MM:ss A');
+    $('#date-time').text(currDateTime);
+}, 1000);
+
+
+function createTable() {
+    let htmlTR = document.createElement("tr")
+    let htmlTD = document.createElement("tr")
+}
+
+function readLocalStorage() {
+    let tempLocal = JSON.parse(localStorage.getItem('custData'));
+    console.log("String: "+ tempLocal);
+    console.log(typeof tempLocal);
+
+    if(!tempLocal) {tempLocal=[];}
+    console.log(tempLocal);
+
+    // let readLocal = {
+    //     name: xxx,
+    //     type: xxx,
+    //     due: xxx
+    // }
+}
+
+const formModal = $('#form-modal');
+const btnModal = $('#btn-form-modal');
+const formDatePicker = $('#project-due');
+
+function showFormModal(event) {
+    console.log("Clicked on button: " + event.target.textContent.trim() )
+    formModal.modal('toggle')
+}
+
+btnModal.on("click", showFormModal);
+formDatePicker.datepicker();
+
+readLocalStorage();
